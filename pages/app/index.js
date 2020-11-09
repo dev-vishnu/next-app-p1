40afd13fa4180eab4b4987e175183a61b59c5622
Vishnu Pillai <vishnu.pillai@dailyhunt.in>
2020-11-10 01:50:39 +0530

    hello world content

--- pages/app/index.js
+++ pages/app/index.js
@@ -57,7 +57,7 @@ const App = () => {
                             minHeight: 280,
                         }}
                     >
-                        Content
+                        Hello World
         </Content>
                 </Layout>
             </Layout>
